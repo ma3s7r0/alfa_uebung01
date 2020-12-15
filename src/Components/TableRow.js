9b84f5712e4120d021e5425df783c9f6db8e2270
Nico <alfatraining@fancydomain.de>
2020-12-15 15:46:40 +0100

    Removed tagging even tr with class 0 instead now class ""

--- src/Components/TableRow.js
+++ src/Components/TableRow.js
@@ -4,7 +4,7 @@ function TableRow(props) {
     return (
         <tr className = {
             //Set on every 2nd row the property odd
-            (props.index & 1) && "odd"
+            (props.index & 1) ? "odd" : ""
         }>{
             //Create a cell for every property of the underlying object
             props.headers.map(header => <td> {props.element[header]} </td>)
