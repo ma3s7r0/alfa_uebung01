0db25b8e24ec1f695148f485368e4b61f335de87
Nico <alfatraining@fancydomain.de>
2020-12-15 09:40:31 +0100

    Ecapsulated the rows in TableRow commented and beautified

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import '../App.css';
+import TableRow from './TableRow';
 
 function Table(props) {
 
     let headers = Object.keys(props.data[0])
 
     return (
-        <>
         <table>
             <tr>
-                {headers.map(header => <th> {header} </th>)}
+                { 
+                //Create the column headers
+                headers.map(header => <th> {header} </th>)
+                }
             </tr>
             {
+                //Scroll through the entries
                 props.data.map(
-                    (element, index) => <tr className={(index & 1) && "odd"}> {headers.map(header => <td>{element[header]}</td>)} </tr>)
+                    (element, index) => (
+                        <TableRow headers={headers} element={element} index={index} />
+                    )
+                )
             }
         </table>
-        </>
     );
 }
 
---
+++ src/Components/TableRow.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TableRow(props) {
+    return (
+        <tr className={
+            //Set on every 2nd row the property odd
+            (props.index & 1) && "odd"
+        }> {
+            //Create a cell for every property of the underlying object    
+            props.headers.map(header => <td> {props.element[header]} </td>)
+            }
+        </tr>    
+        
+    );
+}
+
+export default TableRow;
